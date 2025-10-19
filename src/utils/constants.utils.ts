3d47ef21cbca83cd2f462a032f736361f1713cd2
LeauAlex <leaualex20@gmail.com>
2025-10-19 20:37:55 +0300

    Fixed bug on page reload
    The page reload always took you to the dashboard as result
    of login required instead of using sso

--- src/utils/constants.utils.ts
+++ src/utils/constants.utils.ts
@@ -46,9 +46,12 @@ export const ROUTES = {
 } as const;
 
 export const KEYCLOAK_INIT_OPTIONS: AuthInitOptions = {
-    onLoad: 'login-required',
+    onLoad: 'check-sso',
     pkceMethod: 'S256',
     checkLoginIframe: false,
     responseMode: 'fragment',
     enableLogging: true,
+    silentCheckSsoRedirectUri: typeof window !== 'undefined' 
+        ? window.location.origin + '/silent-check-sso.html' 
+        : undefined,
 };
